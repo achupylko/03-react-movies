ac635b421a8d422fa6d4ffd9624041f2527686d6
Artem Chupylko <achupylko@gmail.com>
2026-03-29 14:28:51 +0000

    fix: update movie service

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -4,15 +4,29 @@ import type { Movie } from '../types/movie';
 const URL = 'https://api.themoviedb.org/3/search/movie';
 const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_TOKEN}`,
-  },
-};
+interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
 
 export default async function getMovies(query: string): Promise<Movie[]> {
-  const response = await axios.get<Movie[]>(`${URL}?${query}`, options);
-  return response.data;
+  if (!query.trim()) {
+    return [];
+  }
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+    params: {
+      query,
+    },
+  };
+  const response = await axios.get<MoviesResponse>(URL, options);
+
+  return response.data.results;
 }
